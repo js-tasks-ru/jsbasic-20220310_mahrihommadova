3318dc36087b9fc4bf56952d5ebec77538f1d9a5
Myahri Hommadova <mahrihommadova@gmail.com>
2022-03-21 17:42:04 +0300

    1st task of 2nd module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (let key in salaries) {
+    if (isNaN(salaries[key]) || salaries[key] == Infinity || salaries[key] == -Infinity) {
+    result += 0;
+    } else if (typeof salaries[key] == "number"){
+      result += salaries[key];
+    }
+  }
+  return result;
 }
