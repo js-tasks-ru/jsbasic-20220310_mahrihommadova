bc97f466e49a742815dec6a5ae128f032bf214a6
Myahri Hommadova <mahrihommadova@gmail.com>
2022-03-28 15:32:27 +0300

    3rd task of 3rd module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  let delIndex;
+  let arr = (str.split(''));
+  for (let i = 0; i < arr.length; i++){
+    if(arr.includes('-')){
+        delIndex = arr.indexOf('-');
+        arr.splice(delIndex, 1);
+        arr[delIndex] = arr[delIndex].toUpperCase();
+    }
+  }
+  str = arr.join('');
+  return str;
 }
