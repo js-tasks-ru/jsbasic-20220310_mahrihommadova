d73101495034e91db98828cfe30a26d3e8c68cbf
Myahri Hommadova <mahrihommadova@gmail.com>
2022-04-03 19:43:55 +0300

    1st task of 4th module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,18 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  ul.className = "jsArray";
+  
+  const newArr = [];    
+  for (let i = 0; i < friends.length; i++) {
+    newArr.push(`${friends[i].firstName} ${friends[i].lastName}`);
+  }
+  
+  for (const person of newArr) {
+    const newList = document.createElement('li');
+    newList.classList.add('new-person');
+    newList.textContent = person;
+    ul.append(newList);
+  }
+    
+  return ul;
 }
