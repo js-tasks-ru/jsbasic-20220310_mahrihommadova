b966fde833ea21e102bdf1b21cd7ec2442f40dae
Myahri Hommadova <mahrihommadova@gmail.com>
2022-04-05 03:15:18 +0300

    Added 2nd task of 5th module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let txt = document.querySelector('#text');
+  btn.addEventListener('click', () => {
+    if(txt.hidden == false) {
+      txt.hidden = true;
+    } else if(txt.hidden == true){
+      txt.hidden = false;
+    } 
+  });
 }
