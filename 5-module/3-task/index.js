174e870b23c9ed7ee91d73f2a0e762feb1013ece
Myahri Hommadova <mahrihommadova@gmail.com>
2022-04-11 18:10:24 +0300

    Added 3rd task of 5th module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
-  // ваш код...
+  let counter = 1; let currentWidth = 0;
+  let slideElem = document.querySelector('.carousel__inner');
+
+  const lftBtn = document.querySelector('.carousel__arrow_left');
+  lftBtn.style.display = 'none';
+  lftBtn.addEventListener('click', () => {
+    rgtBtn.style.display = '';
+    if(counter > 1) {
+      lftBtn.style.display = '';
+      currentWidth -= slideElem.offsetWidth;
+      slideElem.style.transform = `translateX(-${currentWidth}px)`;  
+      counter -= 1;   
+      console.log(`counter = ${counter} and width = ${currentWidth} offsetWidth = ${slideElem.offsetWidth}`);
+    }
+    if(counter == 1) {
+      lftBtn.style.display = 'none';
+    }
+  });
+
+  const rgtBtn = document.querySelector('.carousel__arrow_right');
+  rgtBtn.addEventListener('click', () => {
+    lftBtn.style.display = '';
+    if(counter < 4) {
+      rgtBtn.style.display = '';
+      currentWidth += slideElem.offsetWidth;
+      slideElem.style.transform = `translateX(-${currentWidth}px)`;  
+      counter += 1;   
+      console.log(`counter = ${counter} and width = ${currentWidth} offsetWidth = ${slideElem.offsetWidth}`);
+    }
+    if(counter == 4) {
+      rgtBtn.style.display = 'none';
+    }
+  });
 }
